0d40832d22c48da03339707f32af1e7a20a5f08d
jhchalegre <jhchalegre@gmail.com>
2026-06-20 21:04:37 -0300

    bc completo, revisar ls

--- sha256-vhdl/src/bc.vhdl
+++ sha256-vhdl/src/bc.vhdl
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.sha256_pack.all;
 
 entity bc is
 	port(
@@ -8,19 +9,77 @@ entity bc is
         rst_a       : in std_logic;
         start       : in std_logic;
         status      : in std_logic_vector(6 downto 0);
-        init_load   : out std_logic;
-        sched_en    : out std_logic;
-        comp_en     : out std_logic;
-        out_en      : out std_logic;
+        commands    : out commands;
         done        : out std_logic
 	);
 end entity bc;
 
 architecture structure of bc is
+        TYPE state_type is (IDLE, INIT, SCHED, COMP, DONE_STATE);
+        SIGNAL state, next_state : state_type;
 begin
-	process is
-	begin
-		report "Hello world";
-		wait;
-	end process;
+	reg_estado : process(clk, rst_a)
+        begin
+            if rst_a = '1' then
+                state <= IDLE;
+            elsif rising_edge(clk) then
+                state <= next_state;
+            end if;
+        end process;
+
+        lpe : process(state, start, status)
+        begin
+            case state is
+                when IDLE =>
+                        if start = '1' then
+                            next_state <= INIT;
+                        else
+                            next_state <= IDLE;
+                        end if;
+                when INIT =>
+                        next_state <= SCHED;
+                when SCHED =>
+                        if status(6) = '1' then
+                            next_state <= COMP;
+                        else
+                            next_state <= SCHED;
+                        end if;
+                when COMP =>
+                        if status(6) = '0' then
+                                next_state <= DONE_STATE;
+                                else
+                                next_state <= COMP;
+                                end if;
+                when DONE_STATE =>
+                        next_state <= IDLE;
+                when others =>
+                        next_state <= IDLE;
+            end case;
+        end process;
+
+        ls : process(state)
+        begin
+            case state is
+                when IDLE =>
+                        commands <= (init_load => '0', sched_en => '0', comp_en => '0', out_en => '0');
+                        done <= '0';
+                when INIT =>
+                        commands <= (init_load => '1', sched_en => '0', comp_en => '0', out_en => '0');
+                        done <= '0';
+                when SCHED =>
+                        commands <= (init_load => '0', sched_en => '1', comp_en => '0', out_en => '0');
+                        done <= '0';
+                when COMP =>
+                        commands <= (init_load => '0', sched_en => '0', comp_en => '1', out_en => '0');
+                        done <= '0';
+                when DONE_STATE =>
+                        commands <= (init_load => '0', sched_en => '0', comp_en => '0', out_en => '1');
+                        done <= '1';
+                when others =>
+                        commands <= (init_load => '0', sched_en => '0', comp_en => '0', out_en => '0');
+                        done <= '0';
+            end case;
+        end process;
+
 end architecture structure;
+ 
