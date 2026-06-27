06c575c80a9a4be17218351358f487ff730c49b4
Joao Carneiro Haas <rockischplay@gmail.com>
2026-06-26 23:35:52 -0300

    Move scripts location

--- sha256-vhdl/src/scripts/run.sh
+++ sha256-vhdl/src/scripts/run.sh
@@ -4,6 +4,6 @@ DIRNAME="$(cd "$(dirname "$0")" && pwd)/.."
 
 mkdir -p "$DIRNAME/build"
 ( cd "$DIRNAME/build" && ghdl --remove --std=08 )
-( cd "$DIRNAME/build" && ghdl -i --std=08 "$DIRNAME"/src/rtl/*.vhdl )
+( cd "$DIRNAME/build" && ghdl -i --std=08 "$DIRNAME"/rtl/*.vhdl )
 ( cd "$DIRNAME/build" && ghdl -m --std=08 sha256 )
 ( cd "$DIRNAME/build" && ghdl -r --std=08 sha256 )
